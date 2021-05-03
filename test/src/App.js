8230780c3ebf4011c498ee0dc2fba8ffe955836f
Kyle Ortega <kyleortega253@gmail.com>
2021-05-02 18:11:53 -0700

    Added stuff

--- test/src/App.js
+++ test/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import './App.css';
+import Stuff from './components/Stuff'
 
 function App() {
   return (
     <div className="App">
       <h1>This is the app</h1>
+      <Stuff />
     </div>
   );
 }
---
+++ test/src/components/Stuff.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Stuff = () => {
+    return (
+        <div>
+            <h1>Return of Stuff</h1>
+        </div>
+    )
+}
+
+export default Stuff
